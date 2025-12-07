a8affc187c10791fca34be3981ade9b28ff7e571
Karl Gardner <kgardner3300@gmail.com>
2025-12-07 11:58:17 -0600

    update

--- src/data-source.ts
+++ src/data-source.ts
@@ -17,6 +17,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  // synchronize: true,
   logging: process.env.NODE_ENV === 'development',
   entities: [CustomerData],
   migrations: ['./src/migrations/**/*.ts'],
